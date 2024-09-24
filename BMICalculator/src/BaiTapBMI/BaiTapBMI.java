891626fb5591278b08d52755d8fb69dd163e9e2c
DangThanhThuy89 <thuydangthanh89@gmail.com>
2024-09-24 20:48:19 +0700

    chi go vao day

---
+++ BMICalculator/src/BaiTapBMI/BaiTapBMI.java
@@ -0,0 +1,10 @@
+package BaiTapBMI;
+
+public class BaiTapBMI {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ BaiTap01/BaiTap01/src/MyClass/MyClass.java
@@ -0,0 +1,15 @@
+package MyClass;
+
+public class MyClass {
+
+	public static void main(String[] args) {
+
+		int a = 5;
+		int b = 7;
+		int c = a+b;
+		
+		System.out.println("HÔM NAY TÔI HỌC JAVA");
+		System.out.println("TỔNG LÀ: " + c);
+	}
+
+}
